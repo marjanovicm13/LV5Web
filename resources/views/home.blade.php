9389c8777aeb07c1e99c1aa52446548d14d6efae
marjanovicm13 <marjanovicm314@gmail.com>
2023-04-26 23:08:24 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+
+        <a href="/new-task">Dodajte novi rad</a>
+        <br>
+
+        <div class="col-md-8">
+            <div class="card">
+
+            <p>home-blade</p>
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/nastavnik.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+
+        <a href="/new-task">Dodajte novi rad</a>
+        <br>
+
+        <div class="col-md-8">
+            <div class="card">
+
+                @foreach ($students as $student)
+                    <div style="display: flex; justify-content: space-between;">
+                        <h3>{{ $student->name }} - </h3>
+                        <h3>{{ $teachers_tasks[$loop->index]->naziv_rada }}</h3>
+                    </div>
+                @endforeach
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
